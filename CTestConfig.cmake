6f519659370629cfcf1e96530eba0ba5abcf9b7a
Michael Pfeuti <mpfeuti@ganymede.ch>
2012-02-09 22:02:20 +0100

    testing coverage excludes 3dPartyLibs now

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -11,3 +11,15 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "build.ganymede.ch")
 set(CTEST_DROP_LOCATION "/submit.php?project=gul")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+
+set(CTEST_CUSTOM_EXCLUDE 3rdParty
+                         libs)
+#			pugixml.hpp 
+#                         pugixml.cpp
+#                         debug_new.cpp) 
+
+file(WRITE ${CMAKE_BINARY_DIR}/CTestCustom.cmake 
+           "set(CTEST_CUSTOM_COVERAGE_EXCLUDE ${CTEST_CUSTOM_EXCLUDE})")
+
+unset(CTEST_CUSTOM_EXCLUDE)
