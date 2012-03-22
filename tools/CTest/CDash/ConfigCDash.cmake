b05a4b1fd6d25b730ce32f621dc0eac8b43d35a2
Michael Pfeuti <mpfeuti@ganymede.ch>
2012-03-22 10:39:24 +0100

    cdash test

--- tools/CTest/CDash/ConfigCDash.cmake
+++ tools/CTest/CDash/ConfigCDash.cmake
@@ -8,6 +8,7 @@ configure_file(tools/CTest/CDash/Experimental/Release.ctest.in             CDash
 configure_file(tools/CTest/CDash/Continuous/CodingConventions.ctest.in   CDash/Continuous/CodingConventions.ctest  @ONLY)
 configure_file(tools/CTest/CDash/Continuous/Debug.ctest.in               CDash/Continuous/Debug.ctest              @ONLY)
 configure_file(tools/CTest/CDash/Continuous/Release.ctest.in             CDash/Continuous/Release.ctest            @ONLY)
+configure_file(tools/CTest/CDash/Continuous/Continuous.ctest.in             CDash/Continuous/Continuous.ctest            @ONLY)
 
 configure_file(tools/CTest/CDash/Nightly/Doxygen.ctest.in             CDash/Nightly/Doxygen.ctest            @ONLY)
 configure_file(tools/CTest/CDash/Nightly/CodingConventions.ctest.in   CDash/Nightly/CodingConventions.ctest  @ONLY)
