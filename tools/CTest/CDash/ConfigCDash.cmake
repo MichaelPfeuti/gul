2a9e457f5eb3fe8ded255b094a9b64a00e55e7a6
Michael Pfeuti <mpfeuti@ganymede.ch>
2012-03-22 11:10:53 +0100

    cdash continuous and nightly corrected update target

--- tools/CTest/CDash/ConfigCDash.cmake
+++ tools/CTest/CDash/ConfigCDash.cmake
@@ -4,14 +4,8 @@ configure_file(tools/CTest/CDash/Experimental/Doxygen.ctest.in             CDash
 configure_file(tools/CTest/CDash/Experimental/CodingConventions.ctest.in   CDash/Experimental/CodingConventions.ctest  @ONLY)
 configure_file(tools/CTest/CDash/Experimental/Debug.ctest.in               CDash/Experimental/Debug.ctest              @ONLY)
 configure_file(tools/CTest/CDash/Experimental/Release.ctest.in             CDash/Experimental/Release.ctest            @ONLY)
-configure_file(tools/CTest/CDash/Experimental/Experimental.ctest.in             CDash/Experimental/Experimental.ctest            @ONLY)
+configure_file(tools/CTest/CDash/Experimental/Experimental.ctest.in        CDash/Experimental/Experimental.ctest            @ONLY)
 
-configure_file(tools/CTest/CDash/Continuous/CodingConventions.ctest.in   CDash/Continuous/CodingConventions.ctest  @ONLY)
-configure_file(tools/CTest/CDash/Continuous/Debug.ctest.in               CDash/Continuous/Debug.ctest              @ONLY)
-configure_file(tools/CTest/CDash/Continuous/Release.ctest.in             CDash/Continuous/Release.ctest            @ONLY)
-configure_file(tools/CTest/CDash/Continuous/Continuous.ctest.in             CDash/Continuous/Continuous.ctest            @ONLY)
+configure_file(tools/CTest/CDash/Continuous/Continuous.ctest.in CDash/Continuous/Continuous.ctest @ONLY)
 
-configure_file(tools/CTest/CDash/Nightly/Doxygen.ctest.in             CDash/Nightly/Doxygen.ctest            @ONLY)
-configure_file(tools/CTest/CDash/Nightly/CodingConventions.ctest.in   CDash/Nightly/CodingConventions.ctest  @ONLY)
-configure_file(tools/CTest/CDash/Nightly/Debug.ctest.in               CDash/Nightly/Debug.ctest              @ONLY)
-configure_file(tools/CTest/CDash/Nightly/Release.ctest.in             CDash/Nightly/Release.ctest            @ONLY)
+configure_file(tools/CTest/CDash/Nightly/Nightly.ctest.in CDash/Nightly/RNightlyelease.ctest @ONLY)
