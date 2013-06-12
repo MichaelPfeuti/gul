96c327bbd3a2105e49f3fad97a4a73cde6ef6194
Michael Pfeuti <mpfeuti@ganymede.ch>
2013-06-12 21:36:41 +0200

    adpoted new cmake target_include_directories

---
+++ include/context/ContextCL.h
@@ -0,0 +1,76 @@
+#pragma once
+#ifndef _GUL_MEDIA_AUDIO_FRAME_H_
+#define _GUL_MEDIA_AUDIO_FRAME_H_
+/***************************************************************************
+**
+** This file is part of gul (Graphic Utility Library).
+**
+** Copyright (c) 2011-2013 Michael Pfeuti.
+**
+** Contact: Michael Pfeuti (mpfeuti@ganymede.ch)
+**
+**
+** gul is free software: you can redistribute it and/or modify it under the
+** terms of the GNU Lesser General Public License as published by the Free
+** Software Foundation, either version 3 of the License, or (at your option)
+** any later version.
+**
+** gul is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+** FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+** more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with gul. If not, see <http://www.gnu.org/licenses/>.
+**
+**
+** If you have questions regarding the use of this file, please contact
+** Michael Pfeuti at mpfeuti@ganymede.ch.
+**
+***************************************************************************/
+
+#include "gul_export.h"
+#include <cstdint>
+
+namespace gul
+{
+
+  class GUL_EXPORT AudioFrame
+  {
+    public:
+      AudioFrame(void);
+      AudioFrame(int channels, int sampleRate);
+      ~AudioFrame(void);
+
+      void ResizeData(int sampleCount);
+      int16_t *GetData(void);
+      const int16_t *GetData(void) const;
+      int GetDataSize(void) const ;
+
+      int GetChannels(void) const;
+      int GetSampleRate(void) const;
+      int GetSampleCount(void) const;
+
+      void SetPresentationTime(float pts);
+      float GetPresentationTime(void) const;
+
+      void SetFrameIndex(uint64_t index);
+      uint64_t GetFrameIndex(void) const;
+
+    protected:
+      //using Image::operator =;
+
+    private:
+      int m_channels;
+      int m_sampleRate;
+      float m_presentationTime; //!< PTS in seconds
+      uint64_t m_frameIndex;
+      int m_sampleCount;
+      int16_t* m_data;
+      int m_dataSize;
+  };
+
+}
+
+
+#endif
---
+++ src/context/ContextCL.cpp
@@ -0,0 +1,121 @@
+/***************************************************************************
+**
+** This file is part of gul (Graphic Utility Library).
+**
+** Copyright (c) 2011-2013 Michael Pfeuti.
+**
+** Contact: Michael Pfeuti (mpfeuti@ganymede.ch)
+**
+**
+** gul is free software: you can redistribute it and/or modify it under the
+** terms of the GNU Lesser General Public License as published by the Free
+** Software Foundation, either version 3 of the License, or (at your option)
+** any later version.
+**
+** gul is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+** FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+** more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with gul. If not, see <http://www.gnu.org/licenses/>.
+**
+**
+** If you have questions regarding the use of this file, please contact
+** Michael Pfeuti at mpfeuti@ganymede.ch.
+**
+***************************************************************************/
+
+#include "AudioFrame.h"
+#include "Misc.h"
+#include <cstring>
+
+gul::AudioFrame::AudioFrame(void)
+  : m_channels(0),
+    m_sampleRate(0),
+    m_presentationTime(0),
+    m_frameIndex(0),
+    m_sampleCount(0),
+    m_data(nullptr),
+    m_dataSize(0)
+{
+}
+
+gul::AudioFrame::AudioFrame(int channels, int sampleRate)
+  : m_channels(channels),
+    m_sampleRate(sampleRate),
+    m_presentationTime(0),
+    m_frameIndex(0),
+    m_sampleCount(0),
+    m_data(nullptr),
+    m_dataSize(0)
+{
+
+}
+
+gul::AudioFrame::~AudioFrame(void)
+{
+  GUL_DELETE_ARRAY(m_data);
+}
+
+void gul::AudioFrame::SetPresentationTime(float pts)
+{
+  m_presentationTime = pts;
+}
+
+float gul::AudioFrame::GetPresentationTime(void) const
+{
+  return m_presentationTime;
+}
+
+void gul::AudioFrame::SetFrameIndex(uint64_t index)
+{
+  m_frameIndex = index;
+}
+
+uint64_t gul::AudioFrame::GetFrameIndex(void) const
+{
+  return m_frameIndex;
+}
+
+int gul::AudioFrame::GetChannels(void) const
+{
+  return m_channels;
+}
+
+int gul::AudioFrame::GetSampleRate(void) const
+{
+  return m_sampleRate;
+}
+
+void gul::AudioFrame::ResizeData(int sampleCount)
+{
+  int dataSize = sizeof(int16_t)*m_channels*sampleCount;
+  if(dataSize != m_dataSize)
+  {
+    GUL_DELETE_ARRAY(m_data);
+    m_data = new int16_t[m_channels*sampleCount];
+  }
+  m_sampleCount = sampleCount;
+  m_dataSize = dataSize;
+}
+
+int gul::AudioFrame::GetDataSize(void) const
+{
+  return m_dataSize;
+}
+
+int gul::AudioFrame::GetSampleCount(void) const
+{
+  return m_sampleCount;
+}
+
+int16_t* gul::AudioFrame::GetData(void)
+{
+  return m_data;
+}
+
+const int16_t *gul::AudioFrame::GetData(void) const
+{
+  return m_data;
+}
